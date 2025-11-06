373e5fb025d552210fff4f5ee8a9f898181afbf8
opnextgencoderskr-a11y <op.nextgencoderskr@gmail.com>
2025-11-06 21:18:30 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
